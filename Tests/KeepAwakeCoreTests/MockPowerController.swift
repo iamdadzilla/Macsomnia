454c9848aa9d3c3c44906075209a8e273da26ec1
iamdadzilla <40372948+iamdadzilla@users.noreply.github.com>
2026-07-05 14:00:57 +0100

    feat: add PowerControlling protocol, real pmset impl, and mock

---
+++ Tests/KeepAwakeCoreTests/MockPowerController.swift
@@ -0,0 +1,24 @@
+import Foundation
+@testable import KeepAwakeCore
+
+final class MockPowerController: PowerControlling {
+    private(set) var enableCount = 0
+    private(set) var disableCount = 0
+    var enableError: Error?
+    var sleepDisabledToReturn = false
+    var readError: Error?
+
+    func enable() throws {
+        if let enableError { throw enableError }
+        enableCount += 1
+    }
+
+    func disable() throws {
+        disableCount += 1
+    }
+
+    func readSleepDisabled() throws -> Bool {
+        if let readError { throw readError }
+        return sleepDisabledToReturn
+    }
+}
